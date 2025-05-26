da880fc6e0b9f52889f60c95129cbf4a816652c9
Delin <delinjohnshaji@gmail.com>
2025-05-26 11:21:48 +0000

    added version to requirements

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from pymongo import MongoClient
 client_uri=st.secrets['uri']
+print(client_uri)
 client = MongoClient(client_uri)
-db=client['pwnai_output']
+db=client['Pwani_llm_Output']
+st.write(db.list_collection_names())
